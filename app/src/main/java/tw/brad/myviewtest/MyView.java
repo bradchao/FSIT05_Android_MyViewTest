15963ec095c313111a8803dbe7db28198bd01b68
Brad <brad@brad.tw>
2018-10-14 09:52:41 +0800

    v2

--- app/src/main/java/tw/brad/myviewtest/MyView.java
+++ app/src/main/java/tw/brad/myviewtest/MyView.java
@@ -9,9 +9,17 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class MyView extends View {
+    private LinkedList<LinkedList<HashMap<String,Float>>> lines;
+
+
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        lines = new LinkedList<>();
 
         setBackgroundColor(Color.YELLOW);
     }
@@ -20,10 +28,20 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        Log.v("brad", "onDraw()");
+
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(10);
-        canvas.drawLine(0, 0 , 200, 200, paint);
+
+        for (LinkedList<HashMap<String,Float>> line : lines) {
+            for (int i = 1; i < line.size(); i++) {
+                HashMap<String, Float> p0 = line.get(i - 1);
+                HashMap<String, Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"), p0.get("y"),
+                        p1.get("x"), p1.get("y"), paint);
+            }
+        }
 
     }
 
@@ -31,18 +49,27 @@ public class MyView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         float ex = event.getX();
         float ey = event.getY();
+        HashMap<String,Float> point = new HashMap<>();
+        point.put("x", ex); point.put("y", ey);
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                Log.v("brad", "down: " + ex + " x " + ey);
+                LinkedList<HashMap<String,Float>> line = new LinkedList<>();
+                line.add(point);
+                lines.add(line);
                 break;
-            case MotionEvent.ACTION_UP:
-                Log.v("brad", "up: " + ex + " x " + ey);
-                break;
+
             case MotionEvent.ACTION_MOVE:
-                Log.v("brad", "move: " + ex + " x " + ey);
+                lines.getLast().add(point);
                 break;
         }
+        invalidate();
 
         return true; //super.onTouchEvent(event);
     }
+
+    public void clear(){
+        lines.clear();
+        invalidate();
+    }
+
 }
