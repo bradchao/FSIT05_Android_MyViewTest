e9c47dc11d40aa0face54403b6d088ed7eb68b96
Brad <brad@brad.tw>
2018-10-14 10:28:48 +0800

    v3

--- app/src/main/java/tw/brad/myviewtest/MyView.java
+++ app/src/main/java/tw/brad/myviewtest/MyView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -14,25 +15,63 @@ import java.util.LinkedList;
 
 public class MyView extends View {
     private LinkedList<LinkedList<HashMap<String,Float>>> lines;
+    private GestureDetector gd;
+    private Paint paint = new Paint();
 
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
 
         lines = new LinkedList<>();
 
+        gd = new GestureDetector(context, new MyGestureListener());
+
         setBackgroundColor(Color.YELLOW);
     }
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.v("brad", "onDown");
+            return true;    //super.onDown(e);
+        }
+
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float vX, float vY) {
+            if (Math.abs(vX) > Math.abs(vY) + 1000){
+                if (vX > 0){
+                    Log.v("brad", "Right");
+                }else{
+                    Log.v("brad", "Left");
+                }
+            }else if (Math.abs(vY) > Math.abs(vX) + 1000){
+                if (vY > 0){
+                    Log.v("brad", "Down");
+                }else{
+                    Log.v("brad", "Up");
+                }
+            }
+
+            return super.onFling(e1, e2, vX, vY);
+        }
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Log.v("brad", "onDraw()");
-
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
 
         for (LinkedList<HashMap<String,Float>> line : lines) {
             for (int i = 1; i < line.size(); i++) {
@@ -47,24 +86,28 @@ public class MyView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float ex = event.getX();
-        float ey = event.getY();
-        HashMap<String,Float> point = new HashMap<>();
-        point.put("x", ex); point.put("y", ey);
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                LinkedList<HashMap<String,Float>> line = new LinkedList<>();
-                line.add(point);
-                lines.add(line);
-                break;
+//        float ex = event.getX();
+//        float ey = event.getY();
+//        HashMap<String,Float> point = new HashMap<>();
+//        point.put("x", ex); point.put("y", ey);
+//        switch (event.getAction()){
+//            case MotionEvent.ACTION_DOWN:
+//                LinkedList<HashMap<String,Float>> line = new LinkedList<>();
+//                line.add(point);
+//                lines.add(line);
+//                break;
+//
+//            case MotionEvent.ACTION_MOVE:
+//                lines.getLast().add(point);
+//                break;
+//        }
+//        invalidate();
+//
+//        return true; //super.onTouchEvent(event);
 
-            case MotionEvent.ACTION_MOVE:
-                lines.getLast().add(point);
-                break;
-        }
-        invalidate();
 
-        return true; //super.onTouchEvent(event);
+        return gd.onTouchEvent(event);
+
     }
 
     public void clear(){
